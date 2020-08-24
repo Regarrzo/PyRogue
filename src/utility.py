a87891f188342d2552fe2e74d236efdf90fb8c2f
Linus Röber <linusroeber@gmail.com>
2020-08-24 16:02:52 +0200

    Code style fix

--- src/utility.py
+++ src/utility.py
@@ -77,49 +77,49 @@ class Vec2:
         raise IndexError("Index out of range.")
 
     def copy(self):
-        '''
+        """
         Copies the current vector and returns a new one
-        '''
+        """
         return Vec2(self.x, self.y)
 
     def element_mul(self, other):
-        '''
+        """
         Multiplies two vectors element-wise
-        '''
+        """
         return Vec2(self.x * other.x, self.y * other.y)
 
     def dot(self, other):
-        '''
+        """
         Returns the dot product of two vectors.
-        '''
+        """
         return self.x * other.x + self.y * other.y
 
     def no_root_norm(self):
-        '''
+        """
         Calculates the norm of the vector without taking the square root. This
         is useful for quickly comparing two vectors, when the actual length
         isn't important.
-        '''
+        """
 
         return self.x ** 2 + self.y ** 2
 
     def norm(self):
-        '''
+        """
         Calculates the norm or length of the vector.
-        '''
+        """
         return self.no_root_norm() ** 0.5
 
     def normalize(self):
-        '''
+        """
         Return a new vector representing the normalized current vector.
-        '''
+        """
         norm = self.norm()
         return Vec2(self.x / norm, self.y / norm)
 
     def normalize_inplace(self):
-        '''
+        """
         Normalize the current vector in place, doesn't create a new vector.
-        '''
+        """
         norm = self.norm()
 
         self.x /= norm
